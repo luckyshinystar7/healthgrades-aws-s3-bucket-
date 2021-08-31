d45fce24a10fe6fa0b5b0ffc1d2b4f1269403d87
luckyshinystar7 <luckyshinystar7@gmail.com>
2021-08-31 00:56:37 -0600

    resize width

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ export default class Navbar extends Component {
       <nav className="navbar" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
           <a className="navbar-item" href="/">
-            <img src="BlueEnergy.png" width="112" height="275" alt="hexal logo" />
+            <img src="BlueEnergy.png" width="95" height="275" alt="hexal logo" />
           </a>
         </div>
 
